b4c9f5013c0230f36b81b0eff980c15ed20ab2d9
Taramaurya <tara.ecolods@gmail.com>
2022-12-28 15:51:08 +0530

    mutipleImageUpload28dec22

--- src/screens/HomeScreen/postScreen/SharePost.js
+++ src/screens/HomeScreen/postScreen/SharePost.js
@@ -36,7 +36,7 @@ const  Sharepost = () => {
       status:"1",
       broadcast_to:"",
       postType:"",
-      postImage:"",
+      postImage:[],
       type:"",
       custspeciality:""
   });
@@ -55,7 +55,8 @@ const  Sharepost = () => {
     id:'',
     circle_type:'',
     city_id:''
-  })
+  });
+  const [uploadImage, setuploadImage]   = useState({pimage:[]});
   const [recording, setRecording] = useState()
   const bottomSheetModalRef       = useRef(null);
   const bottomSheetModalRefSecond = useRef(null);
@@ -80,43 +81,56 @@ const  Sharepost = () => {
 
   const pickImage =  () => {
     PickImageAll().then(async (res) =>{
-      console.log("res",res);
+      console.log("resImage",res);
       setData(res);
-      return;
-      let localUri = res?.uri;
-      console.log("localUri",localUri);
-      
-      let filename = localUri.split('/').pop();
-      let uriParts = localUri.split('.');
-      let fileType = uriParts[uriParts.length - 1];
-      let formData = new FormData();
-      const imageData = {
-        uri : localUri,
-        name: filename,
-        type: `video/${fileType}`,
-      }
-      formData.append('postImage', imageData);
-      formData.append('post_id', '3032');
-      const responce = await fetch(`${mainApi.baseUrl}/ApiController/postuploadDocsReact`, {
-        method : 'POST',
-        headers:{
-            'Content-Type': 'multipart/form-data'
-        },
-        body :formData
-     });
-    const result1=  await responce.json();
-      setPost({...post, 
-        postImage: result1.postImage,
-        type:'i'
-
+      var arrayLength = res.length
+      console.log("countData",arrayLength);
+      setloader(true);
+      res.map(async(data) => {
+        // console.log("returnURI",data.uri);
+        let localUri = data.uri
+        // console.log("localUri",localUri);
+        let filename = localUri.split('/').pop();
+        let uriParts = localUri.split('.');
+        let fileType = uriParts[uriParts.length - 1];
+        let formData = new FormData();
+        const imageData = {
+          uri : localUri,
+          name: filename,
+          type: `image/${fileType}`,
+        }
+        formData.append('postImage', imageData);
+        formData.append('post_id', '3032');
+        console.log("formData",formData);
+        // return 
+        const responce = await fetch(`${mainApi.baseUrl}/ApiController/postuploadDocsReact`, {
+          method : 'POST',
+          headers:{
+              'Content-Type': 'multipart/form-data'
+          },
+          body :formData
+       });
+       const result1=  await responce.json();
+     //  console.log("result1",result1);
+        setuploadImage({...uploadImage,
+          uploadImage:uploadImage.pimage.push(result1.postImage)
+        })
+        setPost({...post, 
+          type:'i'
+        });
+      //console.log("postdataShow", uploadImage);
       });
+      setloader(false);
     })
   };
 
-  const asda = pickedData?.map(data => data.uri)
-  console.log("asda",asda);
+  // const asda = pickedData?.map(data => data.uri)
+  // console.log("asda",asda);
 
   const pickVideo =  () => {
+   console.log("resvideo",res);
+    setData(res);
+    // return;
     PickVideos().then(async (res) =>{
       let localUri = res?.uri;
       setData(localUri)
@@ -127,12 +141,13 @@ const  Sharepost = () => {
       const imageData = {
         uri : localUri,
         name: filename,
-        type: `image/${fileType}`,
+        type: `video/${fileType}`,
       }
      // console.log(imageData);
       formData.append('postImage', imageData);
       formData.append('post_id', '3032');
-      //console.log("formData",formData);
+     // console.log("formData",formData);
+      // return 
       const responce = await fetch(`${mainApi.baseUrl}/ApiController/postuploadDocsReact`, {
         method : 'POST',
         headers:{
@@ -141,7 +156,8 @@ const  Sharepost = () => {
         body :formData
      });
     const result1=  await responce.json();
-    console.log(result1);
+    //console.log(result1);
+
       setPost({...post, 
         postImage: result1.postImage,
         type:'v'
@@ -188,8 +204,8 @@ const publishCheck1 = (e, text)=>{
 
 
   const handleStudentSubmit = async() =>{
-    // console.log("postDAta",post);
-    // console.log("userData",userdata);
+    console.log("postDAta",post);
+    console.log("uploadImage",uploadImage);
     if(post.publishto ==''){
       Toast.show('Please Select Publish to');
       bottomSheetModalRefSecond.current?.present();
@@ -199,7 +215,7 @@ const publishCheck1 = (e, text)=>{
       Toast.show("Please Select PostType");
       bottomSheetModalRef.current?.present();
     }else{
-      const uploadData = {userdata,post};
+      const uploadData = {userdata,post,uploadImage};
       setloader(true);
       const result = await dispatch(postCreate(uploadData));
         if(result.payload.status == 'Success'){
@@ -217,16 +233,16 @@ const publishCheck1 = (e, text)=>{
     }
 
   const uploadPostImage = async (post_id) => {
-    console.log(post_id);
+    //console.log(post_id);
     let localUri = {pickedData};
-    console.log(localUri);
+    //console.log(localUri);
     let filename = localUri.split('/').pop();
     log(filename);
     // Infer the type of the image
     let uriParts = localUri.split('.');
-    console.log('uri', uriParts);
+    //console.log('uri', uriParts);
     let fileType = uriParts[uriParts.length - 1];
-    console.log("fileType",fileType );
+    //console.log("fileType",fileType );
     let formData = new FormData();
     const imageData = {
       uri : localUri,
@@ -243,7 +259,7 @@ const publishCheck1 = (e, text)=>{
       body :formData
    });
   const result1 = await responce.json();
-  console.log(result1);
+ // console.log(result1);
 
   Toast.show(result1.payload.message);
   setPost('');
@@ -343,9 +359,9 @@ setSpecialNames(specialityName)
   //   console.log('Recording stopped and stored at', uri);
   // }
   const removeImg = (id) => {
-    console.log("id",id);
+   // console.log("id",id);
     const removed = pickedData?.filter(data => data.assetId != id); 
-    console.log("removed",removed);
+  //  console.log("removed",removed);
     setData(removed);
   }
   return (
